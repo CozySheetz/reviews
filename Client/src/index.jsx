05e75b3a39772c5324d543839da924ca235bbf51
Elizabeth Lang <elizabethlang@Elizabeths-MacBook-Pro-51793.local>
2018-11-03 17:14:50 -0400

    cleaned up client/index.js

--- Client/src/index.jsx
+++ Client/src/index.jsx
@@ -5,74 +5,62 @@ import Reviews from './components/Reviews.jsx';
 import Ratings from './components/Ratings.jsx';
 import Search from './components/Search.jsx';
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       reviews: [],
       averages: [],
       filtered: false,
-      filteredReview : []
+      filteredReviews : [],
+      id: ''
     };
-    this.handleGettingAverages = this.handleGettingAverages.bind(this);
+    this.getAverages = this.getAverages.bind(this);
     this.getUserInformation = this.getUserInformation.bind(this);
     this.handleSearch =this.handleSearch.bind(this);
+    this.getIdFromURL= this.getIdFromURL.bind(this); 
   }
   
+  async componentDidMount ()  {
+      await this.getIdFromURL(); 
+      this.getUserInformation();
+      this.getAverages(); 
+  }
 
-  handleGettingAverages() {
-  
+  getIdFromURL() {
     var path = window.location.href;
     var splits = path.split('/');
     var id = parseInt(splits[splits.length - 1]);
-
-
-    axios.get(`http://18.224.94.179/rating/${id}`).then((response) => {
- 
-      // console.log('Dot data',data.data)
-      this.setState({
-        averages: response
-      });
+    this.setState({id}); 
+  }
+  
+  getAverages() {
+    axios.get(`/rating/${this.state.id}}`).then(({data}) => {
+      this.setState({averages: data});
     });
   }
 
   getUserInformation() {
-    var path = window.location.href;
-    var splits = path.split('/');
-    var id = parseInt(splits[splits.length - 1]);
-    axios.get(`http://18.224.94.179/user/${id}`).then(data => {
-      this.setState({reviews: data.data});
+    axios.get(`/user/${this.state.id}`).then(({data})=> {
+      this.setState({reviews: data});
     });
   }
 
-  handleSearch(filter) {
-   this.setState({filteredReview: filter, filtered: true});
-  
+  handleSearch(filteredReviews) {
+   this.setState({filteredReviews, filtered: true});
   }
-
-  componentDidMount() {
-    this.getUserInformation();
-    this.handleGettingAverages(); 
-  }
-
 
   render() {
     return (
       <div className="app">
-        <header className="navbar">
-        </header>
-
         <div className="main container w-50">
-    
           <Search  search={this.handleSearch} reviewLength={this.state.reviews.length}/> 
           <Ratings averages={this.state.averages}  /> 
-          <Reviews rev={this.state.filtered === false ? this.state.reviews: this.state.filteredReview} />
-
+          <Reviews rev={this.state.filtered === false ? this.state.reviews: this.state.filteredReviews} />
         </div>
       </div>
     );
   }
 }
 
-// window.Reviews = App; 
 
 ReactDOM.render(<App/>, document.getElementById('reviews-app'))
--- db/index.js
+++ db/index.js
@@ -12,6 +12,7 @@ var connection = mysql.createConnection({
     database: 'review',
     port:3306
 });
+
 connection.connect(function (err) {
     if (err) {
         console.error('error', err);
@@ -19,49 +20,49 @@ connection.connect(function (err) {
     }
 
     console.log('Connected to mysql');
-})
+});
 
 module.exports = {
 
     getOverallAverage: (params,cb) => {
-        let info= `SELECT (AVG(accuracy) + AVG(communication) + AVG(cleanliness) + AVG(locat) + AVG(checkIn) + AVG(val) )/6 'averages'FROM reviews WHERE listingId= (?)`
-        connection.query(info,params, (err, data) => {
+        let info= `SELECT (AVG(accuracy) + AVG(communication) + AVG(cleanliness) + AVG(locat) + AVG(checkIn) + AVG(val) )/6 'averages' FROM reviews WHERE listingId= (?)`;
+        connection.query(info, params, (err, data) => {
             if (err) {
                 console.error(err);
             } else {
                 cb(null, data);
             }
-        })
+        });
     },
 
     getRating: (params,cb) => {
-        let info = `SELECT AVG(accuracy)'Accuracy', AVG(communication) 'Communication', AVG(cleanliness) 'Cleanliness', AVG(locat) 'Location', AVG(checkIn) 'Check-in', AVG(val) 'Value' FROM reviews WHERE listingId= (?)`
+        let info = `SELECT AVG(accuracy)'Accuracy', AVG(communication) 'Communication', AVG(cleanliness) 'Cleanliness', AVG(locat) 'Location', AVG(checkIn) 'Check-in', AVG(val) 'Value' FROM reviews WHERE listingId= (?)`;
         connection.query(info, params, (err, data) => {
             if (err) {
                 console.error(err);
             } else {
                 cb(null, data);
             }
-        })
+        });
     },
 
 
-    getJoinInformation: (params, cb) => {
+    getJoinInformation: (listingId, cb) => {
+        let info = `SELECT first_name,last_name, picture, dateCreated, overview from user u inner join reviews r on u.id=r.userId WHERE listingId= (?) ORDER BY r.dateCreated DESC`;
 
-        let info = `SELECT first_name,last_name, picture, dateCreated, overview from user u inner join reviews r on u.id=r.userId WHERE listingId= (?) ORDER BY r.dateCreated DESC`
-
-        connection.query(info, params, (err, data) => {
+        connection.query(info, listingId, (err, data) => {
             if (err) {
                 console.log('error in sql get join', err);
             } else {
                 cb(null, data);
             }
-        })
+        });
 
     },
     getSearchInformation : (params,cb) => {
     
-        let info = `SELECT first_name,last_name, picture, dateCreated, overview from user u inner join reviews r on u.id=r.userId WHERE (listingId = (?) AND MATCH (overview) AGAINST (? IN NATURAL LANGUAGE MODE))`
+        let info = `SELECT first_name,last_name, picture, dateCreated, overview from user u inner join reviews r on u.id=r.userId WHERE (listingId = (?) AND MATCH (overview) AGAINST (? IN NATURAL LANGUAGE MODE))`;
+        
         connection.query(info, params, (err, data) => {
             if (err) {
                 console.log('error in sql get search', err);
@@ -70,6 +71,6 @@ module.exports = {
                 cb(null, data);
             }
       
-        })
+        });
     }
 }
